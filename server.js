0ecf383ee8baef47b85131c960eaec3beb361441
rmonteroh <rmonteroh90@gmail.com>
2020-02-13 13:02:44 -0500

    Inserting data in mlab database

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const Mongoose = require('mongoose');
+const BodyParser = require('body-parser');
+const uri = "mongodb+srv://root:123@cluster0-vtyiw.mongodb.net/test?retryWrites=true&w=majority";
+
+Mongoose.connect(uri, {useNewUrlParser: true}, function(error){
+    if (error) {
+        console.log('Error ', error);
+    } else {
+        console.log('conectado a la db');
+    }
+})
+
+const app = express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+const port = 3001;
+
+
+
+const Event = Mongoose.model("event", {
+        id: String,
+        activityDate: String,
+        eventData: String,
+        eventType: String,
+        shouldBeSave: Boolean,
+        captureData: Object
+});
+
+/* const EventSchema = new Mongoose.Schema({
+    id: String,
+    activityDate: String,
+    eventData: String,
+    eventType: String,
+    shouldBeSave: Boolean,
+    captureData: Object
+});
+var Event = Mongoose.model('Event', EventSchema); */
+
+
+app.get('/', (req, res) => res.send('Hello world'));
+app.post('/event', async (req, res) => {
+    try {
+        //var event = new Event(req.body);
+        // var result = event.save();
+        Event.create(
+            req.body,
+            (error, data) => {
+                if (error) {
+                    console.log('Error creando el evento');
+                    console.log(error);
+                } else {
+                    console.log('Evento creado correctamente');
+                    console.log(data);
+                    res.send(data);
+                }
+            }
+        );
+
+        res.send(result);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
